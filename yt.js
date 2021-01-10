e80ffd05b94d06b7c79983c4f96c2223fef068e6
Giancarlo <gian.guerra23@gmail.com>
2021-01-10 13:24:31 -0500

    Terminamos el menu lateral

--- yt.js
+++ yt.js
@@ -1,1 +1,9 @@
+const contenedor=document.querySelector('#contenedor');
+
+
+document.querySelector('#boton-menu').addEventListener('click',()=>{
+    contenedor.classList.toggle('active');
+})
+
+
 
