c9093aeba6f6804cdbf119c2285b479038eb02b6
Giancarlo <gian.guerra23@gmail.com>
2021-01-10 22:19:27 -0500

    terminamos el proyecto

--- yt.js
+++ yt.js
@@ -7,3 +7,27 @@ document.querySelector('#boton-menu').addEventListener('click',()=>{
 
 
 
+// window.addEventListener('resize',()=>{
+//     if (window.innerWidth<=768) {
+//         contenedor.classList.remove('active');
+//     } else {
+//         contenedor.classList.add('active');
+//     }
+// })
+// Este evento trae un problema; en el supuesto de que por defecto
+// el sitio se ve en una pantalla pequeña, jamas de encogera(no habra evento)
+// entonces no desaparecera el menu.
+
+const comprobarAncho=()=>{
+    if (window.innerWidth<=768) {
+                contenedor.classList.remove('active');
+    } else {
+               contenedor.classList.add('active');
+        }
+}
+
+comprobarAncho();
+
+window.addEventListener('resize',()=>{
+    comprobarAncho();
+})
